41900ebc0653fb5d1c8951e3106a0fc45081ca28
j-hmoon <moonjiho0706@gmail.com>
2025-03-09 19:39:09 +0900

    Add files via upload

---
+++ 01_HelloWindow.js
@@ -0,0 +1,54 @@
+// Global constants
+const canvas = document.getElementById('glCanvas'); // Get the canvas element 
+const gl = canvas.getContext('webgl2'); // Get the WebGL2 context
+
+if (!gl) {
+    console.error('WebGL 2 is not supported by your browser.');
+}
+
+// Set canvas size: 현재 window 전체를 canvas로 사용
+canvas.width = 500;
+canvas.height = 500;
+
+// Initialize WebGL settings: viewport and clear color
+
+gl.enable(gl.SCISSOR_TEST);
+
+// Start rendering
+render();
+
+// Render loop
+function render() {
+    gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.scissor(0, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.scissor(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
+    gl.clearColor(0.0, 1.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.viewport(0, 0, canvas.width/2, canvas.height/2);
+    gl.scissor(0, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+    gl.scissor(canvas.width/2, 0, canvas.width/2, canvas.height/2);
+    gl.clearColor(1.0, 1.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    
+    // Draw something here
+}
+
+// Resize viewport when window size changes
+window.addEventListener('resize', () => {
+    const size = Math.min(window.innerWidth, window.innerHeight);
+    canvas.width = size;
+    canvas.height = size;
+    
+    render();
+});
+
